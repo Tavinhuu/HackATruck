9d912fac5b0af773ec2c9ef487d8df580f64c692
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2025-04-22 21:05:25 -0300

    Desafio 4 - Velocidade media com animais
    Calculadora de velocidade media com animais

---
+++ Desafio 4 - Velocidade media com animais/Desafios-HACKA/Desafios_HACKAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafios_HACKAApp.swift
+//  Desafios-HACKA
+//
+//  Created by Turma02-11 on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafios_HACKAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
